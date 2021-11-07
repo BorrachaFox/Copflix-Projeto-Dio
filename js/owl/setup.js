e9d3c9ffcc1a807b3316a50a54550ad69138cf4c
BorrachaFox <daviantoniosantos@outlook.com>
2021-11-06 23:25:46 -0300

    First commit

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:3
+        },
+        1280:{
+            items:5
+        }
+    }
+})
